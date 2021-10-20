1ef88bd9e31588fb882b95d00dca3218e1f2ae55
KIM CHAN HEE <superlipbalm@gmail.com>
2021-10-21 01:23:45 +0900

    BOJ : 11650 - 좌표 정렬하기

---
+++ BOJ/boj_11650_좌표 정렬하기.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  const result = solution();
+  console.log(result);
+  process.exit();
+});
+
+function solution() {
+  input.shift();
+  return input
+    .map((el) => el.split(' ').map((el) => +el))
+    .sort(([x1, y1], [x2, y2]) => (x1 === x2 ? y1 - y2 : x1 - x2))
+    .map((el) => el.join(' '))
+    .join('\n');
+}
