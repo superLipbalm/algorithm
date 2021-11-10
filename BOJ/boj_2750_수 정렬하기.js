e1ee2a820b2b1172fb57ff5879234e3a66c794a4
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-10 23:49:43 +0900

    BOJ : 2750번 수 정렬하기

---
+++ BOJ/boj_2750_수 정렬하기.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  const result = solution();
+  console.log(result);
+  process.exit();
+});
+
+function solution() {
+  const numbers = input;
+
+  return input.sort((a, b) => a - b).join('\n');
+}
