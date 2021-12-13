4c37a7ba98f2a51c60f130e16a3a72bc37f1ca9b
KIM CHAN HEE <superlipbalm@gmail.com>
2021-12-13 18:14:21 +0900

    Programmers : 섬 연결하기 [Level3] 풀이 수정

--- programmers/Level 3/programmers_42861_섬 연결하기.js
+++ programmers/Level 3/programmers_42861_섬 연결하기.js
@@ -1,6 +1,30 @@
+// function solution(n, costs) {
+//   let total = 0;
+//   const bridges = [];
+//   const cycleTable = Array.from(Array(n), (v, i) => i);
+
+//   costs.sort((a, b) => a[2] - b[2]);
+
+//   for (const [a, b, cost] of costs) {
+//     const [aCycle, bCycle] = [cycleTable[a], cycleTable[b]];
+//     if (aCycle !== bCycle) {
+//       while (true) {
+//         const cycleIdx = cycleTable.indexOf(bCycle);
+//         if (cycleIdx === -1) break;
+//         cycleTable[cycleIdx] = aCycle;
+//       }
+//       total += cost;
+//     }
+//     if (bridges.length === n - 1) break;
+//   }
+
+//   return total;
+// }
+
 function solution(n, costs) {
   let total = 0;
   const bridges = [];
+  const cycles = {};
   const cycleTable = Array.from(Array(n), (v, i) => i);
 
   costs.sort((a, b) => a[2] - b[2]);
@@ -8,11 +32,16 @@ function solution(n, costs) {
   for (const [a, b, cost] of costs) {
     const [aCycle, bCycle] = [cycleTable[a], cycleTable[b]];
     if (aCycle !== bCycle) {
-      while (true) {
-        const cycleIdx = cycleTable.indexOf(bCycle);
-        if (cycleIdx === -1) break;
-        cycleTable[cycleIdx] = aCycle;
+      if (!(aCycle in cycles)) cycles[aCycle] = [a];
+      if (!(bCycle in cycles)) {
+        cycles[aCycle] = [...cycles[aCycle], b];
+        cycleTable[b] = aCycle;
+      } else {
+        cycles[aCycle] = [...cycles[aCycle], ...cycles[bCycle]];
+        cycles[bCycle].forEach((node) => (cycleTable[node] = aCycle));
+        delete cycles[b];
       }
+
       total += cost;
     }
     if (bridges.length === n - 1) break;
