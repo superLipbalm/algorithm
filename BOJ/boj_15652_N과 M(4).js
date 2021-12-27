9154485af9c118e00bc513a5ef91b00cceea12f3
KIM CHAN HEE <superlipbalm@gmail.com>
2021-12-27 23:19:26 +0900

    BOJ : 15650번 N과 M(4)

---
+++ BOJ/boj_15652_N과 M(4).js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const splitToNumber = (input) => input.split(' ').map((el) => +el);
+
+(async function () {
+  const input = [];
+
+  for await (const line of rl) {
+    input.push(line);
+  }
+  rl.close();
+
+  const [n, m] = splitToNumber(input[0]);
+  const result = [];
+
+  findSequence([]);
+
+  console.log(result.sort().join('\n'));
+
+  process.exit();
+
+  function findSequence(sequence) {
+    const length = sequence.length;
+    if (length === m) return result.push(sequence.join(' '));
+
+    for (let i = 1; i <= n; i++) {
+      if (sequence?.[length - 1] > i) continue;
+      findSequence([...sequence, i]);
+    }
+  }
+})();
