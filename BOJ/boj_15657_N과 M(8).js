7c26e965e9a292fd6495c25a508f7380475eaec6
KIM CHAN HEE <superlipbalm@gmail.com>
2022-01-05 15:45:12 +0900

    BOJ : 15657번 N과 M(8)

---
+++ BOJ/boj_15657_N과 M(8).js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const splitToNumber = (input) => input.split(' ').map((el) => +el);
+
+(async function () {
+  const input = [];
+
+  for await (const line of rl) {
+    input.push(line);
+  }
+  rl.close();
+
+  const [n, m] = splitToNumber(input[0]);
+  const numbers = splitToNumber(input[1]).sort((a, b) => a - b);
+  const result = [];
+
+  findSequence([]);
+
+  console.log(result.join('\n'));
+
+  process.exit();
+
+  function findSequence(sequence) {
+    if (sequence.length === m) return result.push(sequence.join(' '));
+
+    for (let i = 0; i < n; i++) {
+      if (sequence.at(-1) > numbers[i]) continue;
+      findSequence([...sequence, numbers[i]]);
+    }
+  }
+})();
