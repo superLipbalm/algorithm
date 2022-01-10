9d066cee7d71dd9b3498278e48f8d964703c52d8
KIM CHAN HEE <superlipbalm@gmail.com>
2022-01-10 13:11:21 +0900

    BOJ : 2206번 벽 부수고 이동하기 재채점 실패로 인해 다시 품 Array의 shift, push를 이용해 queue처럼 사용했는데 성능문제로 시간초과 발생 링크드 리스트를 이용한 큐를 사용해 통과

--- BOJ/boj_2206_벽 부수고 이동하기.js
+++ BOJ/boj_2206_벽 부수고 이동하기.js
@@ -37,11 +37,12 @@ function solution() {
   return result === Number.MAX_SAFE_INTEGER ? -1 : result;
 
   function move() {
-    const queue = [[0, 0, 1, false]];
+    const queue = new Queue();
+    queue.push([0, 0, 1, false]);
     nMap[0][0][0] = 1;
 
     while (queue.length) {
-      const [x, y, n, breakWall] = queue.shift();
+      const [x, y, n, breakWall] = queue.pop();
 
       d.forEach(([dx, dy]) => {
         const [newX, newY] = [x + dx, y + dy];
@@ -57,5 +58,55 @@ function solution() {
         }
       });
     }
+  }
+}
+
+class Node {
+  constructor(value, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class Queue {
+  fore = null;
+  rear = null;
+  length = 0;
+
+  push(value) {
+    if (this.isEmpty) {
+      this.fore = this.rear = new Node(value);
+    } else {
+      const newNode = new Node(value);
+      this.rear.nextNode = newNode;
+      this.rear = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (this.isEmpty) return -1;
+    const value = this.fore.value;
+    this.fore = this.fore.nextNode;
+    this.length--;
+    return value;
+  }
+
+  get size() {
+    return this.length;
+  }
+
+  get isEmpty() {
+    return this.length === 0;
+  }
+
+  get front() {
+    if (this.isEmpty) return -1;
+    return this.fore.value;
+  }
+
+  get back() {
+    if (this.isEmpty) return -1;
+    return this.rear.value;
   }
 }
